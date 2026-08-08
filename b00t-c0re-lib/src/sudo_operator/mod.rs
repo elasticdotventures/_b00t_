0a868da7847fb31696a759d793165c3cfad490a2
Brian H <brianh@elastic.ventures>
2026-08-08 12:47:32 +0000

    chore(sudo-operator): re-export vetted module

--- b00t-c0re-lib/src/sudo_operator/mod.rs
+++ b00t-c0re-lib/src/sudo_operator/mod.rs
@@ -12,3 +12,4 @@ pub mod vetted;
 pub use checkpoint::{checkpoint_system_state, CheckpointRef};
 pub use governance::{SudoDisposition, SudoGrantConstraint, SudoGrantEvidence, SudoReviewEvent};
 pub use verdict::adversarial_review;
+pub use vetted::{check_vetted, load_vetted_registry, VettedResult, VettedScriptEntry};
